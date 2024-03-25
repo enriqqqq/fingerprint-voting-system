9eb67e565c58163b2306edfe110a15b01ea6b995
Enrique Heryanto <108193211+enriqqqq@users.noreply.github.com>
2024-03-25 17:17:53 +0700

    hardware connection prototype

--- client/src/components/VotingPage/BallotChoices.jsx
+++ client/src/components/VotingPage/BallotChoices.jsx
@@ -6,7 +6,7 @@ function BallotChoices({ voteButtonHandler, ballotChoice, selected }) {
             <div className="flex justify-center items-center h-80 w-72 bg-blue-50 rounded-t"> image placeholder </div>
             <div className="flex flex-col items-center px-4 py-4 bg-white rounded-b gap-4">
                 <div className="text-black text-lg font-medium">{ballotChoice}</div>
-                <button className={`${selected ? "bg-amber-200" : "bg-white"} bg-white hover:brightness-90 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow`} onClick={() => voteButtonHandler(ballotChoice) }>Vote</button>
+                <button className={`${selected ? "bg-amber-200" : "bg-white"} hover:brightness-90 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow`} onClick={() => voteButtonHandler(ballotChoice) }>Vote</button>
             </div>
         </div>
     );
--- client/src/pages/VotingPage.jsx
+++ client/src/pages/VotingPage.jsx
@@ -1,41 +1,66 @@
 import { useState } from 'react';
 import Header from '../components/VotingPage/Header';
 import BallotChoices from '../components/VotingPage/BallotChoices';
+import ErrorModal from '../components/VotingPage/ErrorModal';
 
 const ballotChoices = [
     "Tomato",
     "Potato",
     "Carrot",
     "Cucumber",
-    "Broccoli",
+    "Broccolli",
 ]
 
 function VotingPage() {
     const [choice, setChoice] = useState(ballotChoices[0]);
+    const [device, setDevice] = useState(null);
 
     function handleVoteButton(choice) {
         setChoice(choice);
     }
 
+    async function connectToHardware () {
+        try {
+            const vendorID = 0x1A86; // Arduino Uno (find USBDevice.vendorID in browser's console)
+            const newDevice = await navigator.usb.requestDevice({ filters: [ {vendorId: vendorID} ] });
+            console.log(newDevice); // check vendorID
+
+            navigator.usb.addEventListener('disconnect', () => {
+                setDevice(null);
+            })
+
+            if(newDevice.vendorId !== vendorID) {
+                setDevice(null);
+            } else {
+                setDevice(newDevice);
+            }
+        } catch (error) {
+            setDevice(null);
+        }
+    }
+
+
     return(
-        <div className="flex flex-col bg-gray-100 min-h-screen">
-            <Header/>
+        <>
+            { device ? null : <ErrorModal buttonHandler={ connectToHardware } />}
+            <div className="flex flex-col bg-gray-100 min-h-screen overflow-hidden">
+                <Header/>
 
-            {/* display all ballot choices */}
-            <div className="flex justify-center flex-wrap gap-8 px-8 py-12">
-                {
-                    ballotChoices.map((ballotChoice, index) => {
-                        return <BallotChoices 
-                            key={index} 
-                            voteButtonHandler={handleVoteButton}
-                            ballotChoice={ballotChoice}
-                            selected={choice === ballotChoice}
-                        />
-                    })
-                }
+                {/* display all ballot choices */}
+                <div className="flex justify-center flex-wrap gap-8 px-8 py-12">
+                    {
+                        ballotChoices.map((ballotChoice, index) => {
+                            return <BallotChoices 
+                                key={index} 
+                                voteButtonHandler={handleVoteButton}
+                                ballotChoice={ballotChoice}
+                                selected={choice === ballotChoice}
+                            />
+                        })
+                    }
+                </div>
             </div>
-
-        </div>
+        </>
     )
 }
 
